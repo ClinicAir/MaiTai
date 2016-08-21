bb0f2a0c5879fc223d39309f96ce9de04e2be076
Tor is Here <kittikorn.a@playbasis.com>
2016-08-21 11:53:38 +0700

    db

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/times', function(request, response) {
 var pg = require('pg');
 
 app.get('/db', function (request, response) {
-  pg.connect(progress.env.DATABASE_URL, function(err, client, done) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
